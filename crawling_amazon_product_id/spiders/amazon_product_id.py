68a6007affa6754616be5516e3650a95e9725747
Shun-Ichiro Kaneshiro <pinkroot@gmail.com>
2015-06-13 01:31:29 +0900

    start_urlのミスを修正

--- crawling_amazon_product_id/spiders/amazon_product_id.py
+++ crawling_amazon_product_id/spiders/amazon_product_id.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
 class AmazonProductIdSpider(scrapy.Spider):
     name = "amazon_product_id"
-    allowed_domains = ["www.amazon.co.jp/"]
+    allowed_domains = ["www.amazon.co.jp"]
     start_urls = (
-        'http://www.www.amazon.co.jp//',
+        'http://www.www.amazon.co.jp/',
     )
 
     def parse(self, response):
