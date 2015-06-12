c919d5d1452cf96dc30323348850254427bedc38
Shun-Ichiro Kaneshiro <pinkroot@gmail.com>
2015-06-13 01:21:56 +0900

    startproject

---
+++ crawling_amazon_product_id/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for crawling_amazon_product_id project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'crawling_amazon_product_id'
+
+SPIDER_MODULES = ['crawling_amazon_product_id.spiders']
+NEWSPIDER_MODULE = 'crawling_amazon_product_id.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'crawling_amazon_product_id (+http://www.yourdomain.com)'
